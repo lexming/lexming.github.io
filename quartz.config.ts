ad460a1e0b887cd399b419bde163a9eff6677bed
Alex Domingo <alex.domingo.toro@vub.be>
2026-03-22 02:15:42 +0100

    switch to own fork of quartz with transformer for lucide icons

--- quartz.config.ts
+++ quartz.config.ts
@@ -72,6 +72,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.LucideIcons(),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
