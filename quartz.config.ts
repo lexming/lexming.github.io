c258e82b7d6934f452689e8be0e8eb6201e17fb9
Alex Domingo <alex.domingo.toro@vub.be>
2026-07-16 01:01:21 +0200

    ignore git and filesystem dates

--- quartz.config.ts
+++ quartz.config.ts
@@ -55,7 +55,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "git", "filesystem"],
+        priority: ["frontmatter"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
