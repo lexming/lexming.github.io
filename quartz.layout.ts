8265a18882dbaeb5c81eb7d842a8e1ac4558d6ba
Alex Domingo <alex.domingo.toro@vub.be>
2026-08-08 12:40:52 +0200

    add license in article footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -22,6 +22,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ContentMeta(),
     Component.TagList(),
   ],
+  afterBody: [
+    Component.ArticleFooter(),
+  ],
   left: [
     Component.PageTitle(),
     Component.Flex({
@@ -47,7 +50,11 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+      Component.Breadcrumbs(),
+      Component.ArticleTitle(),
+      Component.ContentMeta(),
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
